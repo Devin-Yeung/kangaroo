8c87c09943c23d0b1ac9cc09d95587b276a62e2d
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2023-10-10 13:17:33 +0800

    feat: dot support for DFA (#1)

---
+++ src/dfa/dot.rs
@@ -0,0 +1,67 @@
+use crate::dfa::core::DFA;
+use askama::Template;
+
+impl DFA {
+    pub fn dot(&self) -> String {
+        let transitions = self
+            .transitions
+            .iter()
+            .map(|((from, via), to)| Transition {
+                from: &from.name,
+                via: *via,
+                to: &to.name,
+            })
+            .collect::<Vec<_>>();
+
+        let accepts = self
+            .accept
+            .iter()
+            .map(|state| state.name.as_str())
+            .collect::<Vec<_>>();
+
+        let template = DFATemplate {
+            transitions,
+            accepts,
+        };
+
+        template.render().unwrap()
+    }
+}
+
+#[derive(Template)]
+#[template(path = "dfa.dot", syntax = "default", escape = "none")]
+pub struct DFATemplate<'a> {
+    transitions: Vec<Transition<'a>>,
+    accepts: Vec<&'a str>,
+}
+
+struct Transition<'a> {
+    from: &'a str,
+    via: char,
+    to: &'a str,
+}
+
+#[cfg(test)]
+mod test {
+    use crate::dfa;
+
+    #[test]
+    pub fn it_works() {
+        let dfa = dfa! {
+            state { q0, q1 }
+
+            start { q0 }
+
+            transition {
+                q0, '0' -> q1,
+                q0, '1' -> q0,
+                q1, '0' -> q0,
+                q1, '1' -> q1,
+            }
+
+            accept { q1 }
+        };
+
+        println!("{}", dfa.dot());
+    }
+}
--- src/dfa/mod.rs
+++ src/dfa/mod.rs
@@ -1,3 +1,4 @@
 pub mod builder;
 pub mod core;
+pub mod dot;
 pub mod macros;
